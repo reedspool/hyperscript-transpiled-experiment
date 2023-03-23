fa9e275d6b4dc3b0c92a093fa0cbc9c04b79bad1
Human <reedwith2es@gmail.com>
2023-03-22 21:20:00 -0700

    Refactor parse tester

--- transpiler.test.js
+++ transpiler.test.js
@@ -1,18 +1,28 @@
-import assert from 'node:assert';
+import { match, strictEqual } from 'node:assert';
 import { describe, it } from 'node:test';
 import * as Parser from './parser.cjs';
 import { transpile } from "./transpiler.js";
 const wrap = (text) => (fn) => (text2, ...args) => fn(text + text2, ...args)
-const given = wrap('Given ')(describe), then = wrap('Then ')(it);
+const given = wrap('Given ')(describe);
+const when = wrap('When ')(it);
+const then = wrap('Then ')(it);
+const t = (program) => transpile(Parser.parse(program))
+const tMatches = (program, regex) => match(t(program), regex);
+const tEquals = (program, value) => strictEqual(t(program), value);
 given("a test suite", () => {
-    then('assertions don\'t fail', () => assert.strictEqual(1, 1))
+    then('assertions don\'t fail', () => strictEqual(1, 1))
 })
 given("a transpiler", () => {
-    then("it provides a transpile function", () => assert.strictEqual(typeof transpile, "function"))
-    then("it transpiles a log command with no text",
-         () => assert.strictEqual(transpile(Parser.parse('log')), "function (target){ console.log() }"))
-    then("it transpiles a log command with text",
-         () => assert.strictEqual(transpile(Parser.parse('log "hello"')), 'function (target){ console.log("hello") }'))
-    then("it transpiles an on-click feature",
-         () => assert.strictEqual(transpile(Parser.parse('on click\nlog "hello"')), 'function (target){ target.addEventListener(\'click\', () => { console.log("hello") }) }'))
+    then(
+        "it provides a transpile function",
+        () => strictEqual(typeof transpile, "function"))
+    then(
+        "it transpiles a log command with no text",
+        tMatches('log', /console\.log\(\)/))
+    then(
+        "it transpiles a log command with text",
+        tMatches('log "hello"', /console\.log\("hello"\)/))
+    then(
+        "it transpiles an on-click feature",
+        tEquals('on click\nlog "hello"', 'function (target){ target.addEventListener(\'click\', () => { console.log("hello") }) }'))
 })
